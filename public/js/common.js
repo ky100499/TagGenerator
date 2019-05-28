5a8a9d734a31f019024259be96ab36a515cc65bc
ky100499 <ky100499@naver.com>
2019-05-29 03:58:13 +0900

    Change style & Make JS easy
    Should make save later

--- public/js/common.js
+++ public/js/common.js
@@ -1,58 +1,76 @@
-Array.from(document.querySelectorAll(".select-wrap.tag > .select-item")).forEach(elem => {
-    elem.addEventListener('click', function() {
-        if (Array.from(this.classList).includes("active")) {
-            this.classList.remove("active")
+let tag = ""
+let tags = document.getElementById("select-tag").children
+for (let i = 0; i < tags.length; i++) {
+    tags[i].addEventListener('click', e => {
+        let classes = tags[i].classList
+        let hasActive = false
+        for (let j = 0; j < classes.length; j++) {
+            if (classes[j] == "active") {
+                hasActive = true
+                break
+            }
+        }
+        if (hasActive) {
+            tags[i].classList.remove('active')
+            tag = ""
         }
         else {
-            Array.from(document.querySelectorAll(".select-wrap.tag > .active")).forEach(item => {
-                item.classList.remove('active')
-            })
-            this.classList.add('active')
+            for (let j = 0; j < tags.length; j++) {
+                tags[j].classList.remove('active')
+            }
+            tags[i].classList.add('active')
+            tag = tags[i].innerText
         }
+        console.log(tag)
     })
-})
+}
 
 function genTag() {
-    let elem
-
-    let selectedElem = document.querySelector(".select-wrap.tag > .active")
-    if (selectedElem == null) {
+    if (tag == "") {
         alert("요소를 선택하세요")
-        return
     }
     else {
-        elem = document.createElement(selectedElem.dataset.tag)
+        let elem = document.createElement(tag)
+
+        bgColor = document.getElementById("color-bg").value
+        fgColor = document.getElementById("color-fg").value
+        elem.style.backgroundColor = bgColor
+        elem.style.color = fgColor
+
+        let attrs = document.getElementById("select-attr").children
+        for (let i = 0; i < attrs.length; i++) {
+            let inputs = attrs[i].children
+            let attr = inputs.attr.value.trim()
+            let val = inputs.val.value
+            if (attr.length > 0) {
+                elem.setAttribute(attr, val)
+            }
+            inputs.attr.value = ""
+            inputs.val.value = ""
+        }
+
+        text = document.getElementById("select-text").value
+        elem.innerText = text
+
+        document.getElementById("result").innerHTML = ""
+        document.getElementById("result").append(elem)
+        document.getElementById("history").prepend(elem.cloneNode(true))
+
+        let actives = document.getElementsByClassName("active")
+        for (let i = 0; i < actives.length; i++) {
+            actives[i].classList.remove("active")
+        }
+        tag = ""
+
+        document.getElementById("color-bg").value = "#000000"
+        document.getElementById("color-fg").value = "#000000"
+
+        for (let i = 0; i < attrs.length; i++) {
+            let inputs = attrs[i].children
+            inputs.attr.value = ""
+            inputs.val.value = ""
+        }
+
+        document.getElementById("select-text").value = ""
     }
-
-    bgColor = document.querySelector("input[name='color-bg']").value
-    fgColor = document.querySelector("input[name='color-fg']").value
-    elem.style.backgroundColor = bgColor
-    elem.style.color = fgColor
-
-    Array.from(document.querySelectorAll(".attr > .select-item")).forEach(e => {
-        let attr = e.children.attr.value.trim()
-        let val = e.children.val.value.trim()
-        if (attr.length > 0)
-            elem.setAttribute(attr, val)
-    })
-
-    text = document.querySelector("textarea[name='text']").value
-    elem.innerText = text
-
-    Array.from(document.getElementById("result").children).forEach(e => {
-        e.remove()
-    })
-    document.getElementById("result").append(elem)
-
-    document.getElementById("history").prepend(document.createElement("br"))
-    document.getElementById("history").prepend(elem.cloneNode(true))
-
-    Array.from(document.querySelectorAll(".select-wrap.tag > .active")).forEach(item => {
-        item.classList.remove('active')
-    })
-    document.querySelector("input[name='color-bg']").value = document.querySelector("input[name='color-fg']").value = "#000000"
-    Array.from(document.querySelectorAll(".attr > .select-item")).forEach(e => {
-        e.children.attr.value = e.children.val.value = ""
-    })
-    document.querySelector("textarea[name='text']").value = ""
 }
